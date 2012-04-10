c4d66d9a4761ab142c19d546503ec8c77692ea2f
Kevin Krüger <kkevin@gmx.net>
2012-04-10 20:31:50 +0200

    added makefile  for android

---
+++ libsimplefft/android.mk
@@ -0,0 +1,12 @@
+# SHA1_Library/jni/Android.mk
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := 
+
+LOCAL_MODULE     := libsimplefft										# name of your module
+LOCAL_LDLIBS     := -L$(SYSROOT)/usr/lib								# libraries to link against
+LOCAL_SRC_FILES  := alloc.c fft.c libsimplefft.c twiddle.c util.c		# list of source files
+
+include $(BUILD_SHARED_LIBRARY)			# uncomment this line to build a shared library
+#include $(BUILD_STATIC_LIBRARY)				# here, we are building a static library
