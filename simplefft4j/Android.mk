0dc8846dc484b087715c4850aa90b4078e9d5c81
Kevin Krüger <kkevin@gmx.net>
2012-04-11 09:48:38 +0200

    added link against libsimplefft

--- simplefft4j/Android.mk
+++ simplefft4j/Android.mk
@@ -4,8 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_CFLAGS := 
 
 LOCAL_MODULE     := simplefft4j					# name of your module
-LOCAL_LDLIBS     := -L$(SYSROOT)/usr/lib -lsimplefft	# libraries to link against
+LOCAL_LDLIBS     := -L$(SYSROOT)/usr/lib	# libraries to link against
 LOCAL_SRC_FILES  := in.waslos_kneo_libsimplefft_FFTProcessor.c		# list of source files
-
+LOCAL_SHARED_LIBRARIES := libsimplefft
 include $(BUILD_SHARED_LIBRARY)			# uncomment this line to build a shared library
 #include $(BUILD_STATIC_LIBRARY)				# here, we are building a static library
