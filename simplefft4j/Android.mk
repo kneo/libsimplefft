94cd37428ac0fb295657d720a2a4c61d1a5cb6c5
Kevin Krüger <kkevin@gmx.net>
2012-04-11 09:23:19 +0200

    added android mk for simplefft4j

---
+++ simplefft4j/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := 
+
+LOCAL_MODULE     := simplefft4j					# name of your module
+LOCAL_LDLIBS     := -L$(SYSROOT)/usr/lib -lsimplefft	# libraries to link against
+LOCAL_SRC_FILES  := in.waslos_kneo_libsimplefft_FFTProcessor.c		# list of source files
+
+include $(BUILD_SHARED_LIBRARY)			# uncomment this line to build a shared library
+#include $(BUILD_STATIC_LIBRARY)				# here, we are building a static library
