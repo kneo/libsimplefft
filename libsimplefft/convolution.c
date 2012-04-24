176e031b4ec2776df4c79dd92b454181b08a2840
Kevin Krüger <kkevin@gmx.net>
2012-04-24 12:39:21 +0200

    fixing

--- libsimplefft/convolution.c
+++ libsimplefft/convolution.c
@@ -54,44 +54,47 @@ void perform_convolution(CONVOLUTION_CONTEXT* context, CPLX_SAMPLES* signal){
 		lsfft_perform(context->fft_context,signal);
 		
 		//integer pointers
-		int16_t* kre = (int16_t*) context->kernel->re;
-		int16_t* kim = (int16_t*) context->kernel->im;
-		int16_t* sre = (int16_t*) signal->re;
-		int16_t* sim = (int16_t*) signal->im;
+		int16_t* ikre = (int16_t*) context->kernel->re;
+		int16_t* ikim = (int16_t*) context->kernel->im;
+		int16_t* isre = (int16_t*) signal->re;
+		int16_t* isim = (int16_t*) signal->im;
 				
 		//float pointers
-		float* kre = (float*) context->kernel->re;
-		float* kim = (float*) context->kernel->im;
-		float* sre = (float*) signal->re;
-		float* sim = (float*) signal->im;
+		float* fkre = (float*) context->kernel->re;
+		float* fkim = (float*) context->kernel->im;
+		float* fsre = (float*) signal->re;
+		float* fsim = (float*) signal->im;
 					
 		//double pointers
-		double* kim = (double*) context->kernel->im;
-		double* kre = (double*) context->kernel->re;
-		double* sre = (double*) signal->re;
-		double* sim = (double*) signal->im;
+		double* dkim = (double*) context->kernel->im;
+		double* dkre = (double*) context->kernel->re;
+		double* dsre = (double*) signal->re;
+		double* dsim = (double*) signal->im;
 		
 		//point wise multiplication
 		int i = 0;
 		switch(context->type){
 			case CPLX_TYPE_INT:			
 				for(;i<context->samples;i++){
-					sre[i] = re_mul_i(sre[i],sim[i],kre[i],kim[i]);
-					sim[i] = im_mul_i(sre[i],sim[i],kre[i],kim[i]);
+					int16_t itmp = isre[i];
+					isre[i] = re_mul_i(isre[i],isim[i],ikre[i],ikim[i]);
+					isim[i] = im_mul_i(itmp,isim[i],ikre[i],ikim[i]);
 				}
 			break;
 			
 			case CPLX_TYPE_SP:
 				for(;i<context->samples;i++){
-					sre[i] = re_mul_f(sre[i],sim[i],kre[i],kim[i]);
-					sim[i] = im_mul_f(sre[i],sim[i],kre[i],kim[i]);
+					float ftmp = sre[i];
+					fsre[i] = re_mul_f(fsre[i],fsim[i],fkre[i],fkim[i]);
+					fsim[i] = im_mul_f(ftmp,fsim[i],fkre[i],fkim[i]);
 				}
 			break;
 			
 			case CPLX_TYPE_DP:
-				for(;i<context->samples;i++){					
-					sre[i] = re_mul_d(sre[i],sim[i],kre[i],kim[i]);
-					sim[i] = im_mul_d(sre[i],sim[i],kre[i],kim[i]);
+				for(;i<context->samples;i++){
+					double dtmp = sre[i];
+					dsre[i] = re_mul_d(dsre[i],dsim[i],dkre[i],dkim[i]);
+					dsim[i] = im_mul_d(dtmp,dsim[i],dkre[i],dkim[i]);
 				}
 			break;
 		}
